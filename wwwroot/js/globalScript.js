8d2eb776a141b13708c8a80792d6b9b0c6ae00c6
Septimis <connor.meads@live.com>
2023-08-06 11:30:35 -0600

    Style updates Now menu becomes a hamburger when view width < 847px.

--- wwwroot/js/globalScript.js
+++ wwwroot/js/globalScript.js
@@ -1,1 +1,14 @@
-document.getElementById('servicesDropDownWrapper').style.left = Number(document.getElementById('servicesLink').offsetLeft).toString() + 'px';
+const servicesDropDown = document.getElementById('servicesDropDownWrapper');
+const servicesLink = document.getElementById('servicesLink');
+
+//Manually set the 'Left' property of the dropdown menu
+servicesDropDown.style.left = Number(servicesLink.offsetLeft).toString() + 'px';
+
+//Functionality for hamburger menu
+const hamburger = document.getElementById('hamburger');
+const linkWrapper = document.getElementById('linkWrapper');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    linkWrapper.classList.toggle('active');
+});
